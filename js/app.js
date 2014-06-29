001c179394a616178749928d3c2b5c1addb8ee97
toluhi <toluhi@yahoo.com>
2014-06-29 20:26:28 +0100

    created login screen

---
+++ js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by yemi-t on 29/06/2014.
+ */
+var app = angular.module('AngBoot', []);
---
+++ js/controllers/loginController.js
@@ -0,0 +1,10 @@
+/**
+ * Created by yemi-t on 29/06/2014.
+ */
+app.controller('loginController', function($scope){
+ $scope.signin = function($scope)
+ {
+     alert("login");
+ }
+});
+

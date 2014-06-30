5abc0ab4b23f38851536c11bd607d626bf7f8fe4
toluhi <toluhi@yahoo.com>
2014-06-30 06:28:50 +0100

    Added angular libs and basic routing that only goes to the login page

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 /**
  * Created by yemi-t on 29/06/2014.
  */
-var app = angular.module('AngBoot', []);
+var app = angular.module('AngBoot', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/login', {templateUrl: './partials/login.html', controller: 'loginController'});
+
+    $routeProvider.otherwise({redirectTo: '/login'});
+}]);
